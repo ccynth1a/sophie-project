1b9eabf5de8d415f6a2dbe886fe01587a10faa1f
CCynth1a <charlottetorvell@protonmail.com>
2025-01-06 12:45:45 +0000

    changes to clock.svelte

--- src/Clock.svelte
+++ src/Clock.svelte
@@ -1,6 +1,14 @@
 <script lang="ts">
+    import { onMount } from "svelte";
+
     let date = new Date();
     let time = $state(date.toLocaleTimeString())
+
+    onMount(() => {
+        setInterval(() => {
+           time = date.getDate().toLocaleString()
+        }, 100);
+    })
 </script>
 
 <span>
