445a72935480ec871d1215cb456cee8b446ca64e
phlobism <phibiii@proton.me>
2025-01-06 10:37:57 +0000

    styling changes round 2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -55,24 +55,26 @@
 <!-- the long bar -->
 <!-- <div class=" bg-black py-8 "></div> -->
 <!-- the container of the main page :3 -->
-<div class=" m-1 ">
-    <!-- the grid container :3 -->
-    <div class=" grid grid-cols-2 grid-rows-3 gap-2 h-[512px] ">
-        <!-- top thang :3 -->
-        <div class=" border-4 border-green-900 rounded-lg p-1 col-span-2 ">
-            CPU USAGE
-        </div>
-        <!-- middle left thang -->
-        <div class=" border-4 border-yellow-500 rounded-lg p-1 col-span-1 ">
-            MEMORY USAGE
-        </div>
-        <!-- bottom right thang :3 -->
-        <div class=" border-4 border-red-400 rounded-lg p-1 col-span-1 row-span-2 ">
-            PROGRAMS
-            <!-- <Chart type="pie" {chartData} {chartOptions} /> -->
-        </div>
-        <div class=" border-4 border-blue-500 rounded-lg p-1 col-span-1 ">
-            NETWORK USAGE
+<div class=" bg-pink-300 h-screen w-full p-3 rounded-xl ">
+    <div class=" p-4 bg-gray-900 text-white rounded-xl">
+        <!-- the grid container :3 -->
+        <div class=" grid grid-cols-2 grid-rows-3 gap-4 h-[900px] ">
+            <!-- top thang :3 -->
+            <div class=" border-4 border-green-900 rounded-lg p-1 col-span-2 ">
+                CPU USAGE
+            </div>
+            <!-- middle left thang -->
+            <div class=" border-4 border-yellow-500 rounded-lg p-1 col-span-1 ">
+                MEMORY USAGE
+            </div>
+            <!-- bottom right thang :3 -->
+            <div class=" border-4 border-red-400 rounded-lg p-1 col-span-1 row-span-2 ">
+                PROGRAMS
+                <!-- <Chart type="pie" {chartData} {chartOptions} /> -->
+            </div>
+            <div class=" border-4 border-blue-500 rounded-lg p-1 col-span-1 ">
+                NETWORK USAGE
+            </div>
         </div>
     </div>
 </div>
