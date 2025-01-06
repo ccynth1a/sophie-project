e3d8cb211c9d3347a456e8c50321d6da13647b9f
phlobism <phibiii@proton.me>
2025-01-06 11:58:46 +0000

    start on clock

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import BarChart from '../BarChart.svelte';
     import PieChart from '../PieChart.svelte';
     import SalesChart from '../SalesChart.svelte';
 
@@ -11,10 +12,12 @@
 <!-- the container of the main page :3 -->
 <div class=" m-4 ">
     <div class=" p-4 bg-gray-800 text-white rounded-xl">
+        <div>11:57</div>
         <!-- the grid container :3 -->
-        <div class=" grid grid-cols-2 grid-rows-3 gap-4 h-[900px] ">
+        <div class=" grid grid-cols-2 grid-rows-3 gap-4 h-[875px] ">
             <!-- top thang :3 -->
             <div class=" border-4 border-green-400 rounded-lg p-4 col-span-2 ">
+                <BarChart />
             </div>
             <!-- middle left thang -->
             <div class=" border-4 border-yellow-400 rounded-lg p-4 col-span-1 ">
