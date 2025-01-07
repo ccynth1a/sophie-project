338c97f76068b5916818350be45e06f884794a61
CCynth1a <charlottetorvell@protonmail.com>
2025-01-07 09:51:34 +0000

    added types.ts

---
+++ src/lib/types.ts
@@ -0,0 +1,10 @@
+export type Sale = {
+    customer_id: number;
+    gender: String;
+    age: number;
+    total_spent: number;
+    items_bought: number;
+    purchase_date: String;
+    location: String;
+    payment_method: String
+}
