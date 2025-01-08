c3fc965198eebe9384b5c18c5f26228263a3f8f0
phlobism <phibiii@proton.me>
2025-01-08 14:43:17 +0000

    loading screen compartmentalised

---
+++ src/Loading.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    let theloaderr = [
+        {
+            value : 0
+        },
+        {
+            value : 1
+        },
+        {
+            value : 2
+        },
+    ]
+</script>
+
+<!-- containererrr -->
+<div class=" fixed top-0 left-0 h-screen w-screen text-transparent grid grid-cols-3 grid-rows-3 ">
+    <div class=" col-start-2 row-start-2 flex flex-row gap-2 items-center  ">
+        <div class=" text-white text-4xl " >Loading...</div>
+        <!-- {#each {length: 40} as _, i}
+            <div class=" bg-blue-400 rounded h-fit text-center py-2 ">I</div>
+        {/each} -->
+    </div>
+</div>
+
+<style>
+
+</style>
