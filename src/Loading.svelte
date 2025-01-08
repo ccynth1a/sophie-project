2b5cddf7420b6ff6c66298d6b70fe41f28cd966b
CCynth1a <charlottetorvell@protonmail.com>
2025-01-08 15:13:06 +0000

    removed dead code

--- src/Loading.svelte
+++ src/Loading.svelte
@@ -1,17 +1,3 @@
-<script lang="ts">
-    let theloaderr = [
-        {
-            value : 0
-        },
-        {
-            value : 1
-        },
-        {
-            value : 2
-        },
-    ]
-</script>
-
 <!-- containererrr -->
 <div class=" fixed top-0 left-0 h-screen w-screen text-transparent grid grid-cols-3 grid-rows-3 ">
     <div class=" col-start-2 row-start-2 flex flex-col items-center  ">
